273f48722fced91739f0dca6f04f21b9ba2b7ad2
mateussdmoura <mateus.ssmoura@gmail.com>
2022-07-28 17:03:45 -0300

    cloud functions, views, excluir file swaps.logs

--- flask/api/views.py
+++ flask/api/views.py
@@ -1,44 +1,37 @@
 from cgitb import handler
 from flask import Blueprint, Response
 from .interactions import exec_copy_trade
-import logging
-from datetime import datetime
 
 views = Blueprint('views', __name__)
 
-logger1 = logging
-logger2 = logging
-
-logger1.basicConfig(filename='./swaps.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
-
-logger2.basicConfig(filename='./swaps.log', level=logging.WARNING, format='%(asctime)s:%(levelname)s:%(message)s')
-
 @views.route('/')
 def home():
-    return '<h2>V1 copy trade bot by mourinha</h2>'
+    return '<h2>copy trade</h2>'
 
 @views.route('/swap/<tokenAddress>')
 def swap_token(tokenAddress):
-    logger1.info("Starting Swap!", "\nToken Address:", str(tokenAddress))
+    print("Starting Swap!") 
+    print("Token Address:", str(tokenAddress))
     swapped, receipt = exec_copy_trade(str(tokenAddress))
 
     if swapped:
-        logger1.info("Success Swap using wallet", receipt["from"])
+        print("Success Swap using wallet", receipt["from"])
         return Response(status=200)
     else:
-        logger2.warning("Fail Swap")
+        print("Fail Swap")
         return Response(status=404)
         
 @views.route('/swap/<tokenAddress>/<gas>')
 def swap_token_with_same_gas(tokenAddress, gas):
-    logger1.info("Starting Swap!", "\nToken Address:", str(tokenAddress))
+    print("Starting Swap!") 
+    print("Token Address:", str(tokenAddress))
     swapped, receipt = exec_copy_trade(str(tokenAddress, gas=gas))
     print()
 
     if swapped:
-        logger1.info("Success Swap using wallet", receipt["from"])
+        print("Success Swap using wallet", receipt["from"])
         return Response(status=200)
     else:
-        logger2.warning("Fail Swap")
+        print("Fail Swap")
         return Response(status=404)
     
--- moralis/cloudFunction.js
+++ moralis/cloudFunction.js
@@ -2,13 +2,13 @@ Moralis.Cloud.afterSave("EthTokenTransfer", async function (request) {
     const confirmed = request.object.get("confirmed");
     const tokenAddress = request.object.get("token_address");
 
-    const url = 45.33.117.243;
+    const HOST = "45.33.117.243";
 
     if (confirmed) {
       
         Moralis.Cloud.httpRequest({
             method: "GET",
-            url: `${url}/swap/${tokenAddress}`
+            url: `${HOST}/swap/${tokenAddress}`
         })
 
     }
